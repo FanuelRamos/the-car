9d45c29a0df00b5e99bb16de6f5fc72a275621dd
Fanuel Ramos <fanuelramos111@gmail.com>
2023-02-12 01:59:12 +0100

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: false,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
